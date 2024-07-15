39b046f0be84e234824362189a0492f6c732c3b2
tophercully <89924670+tophercully@users.noreply.github.com>
2024-07-15 14:05:59 +0200

    loading spinner on builder

--- src/pages/Builder.tsx
+++ src/pages/Builder.tsx
@@ -16,11 +16,13 @@ import {
 import { CategoryInfo } from "../utils/categories";
 import { detectLanguage } from "../utils/detectLanguage";
 import { track } from "@vercel/analytics";
+import { LoadingSpinner } from "../components/LoadingSpinner";
 
 export const Builder = () => {
   const [message, setMessage] = useState<string | null>(null);
   const navigate = useNavigate();
   const [categories, setCategories] = useState<CategoryInfo[] | undefined>([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   const { snippetId } = useParams();
   if (snippetId) {
@@ -71,8 +73,16 @@ export const Builder = () => {
   useEffect(() => {
     if (isEditing && snippetId) {
       const loadSnippet = async () => {
-        const fetchedSnippet = await loadSnippetById(Number(snippetId));
-        setSnippet(fetchedSnippet as Snippet);
+        setIsLoading(true);
+        try {
+          const fetchedSnippet = await loadSnippetById(Number(snippetId));
+          setSnippet(fetchedSnippet as Snippet);
+        } catch (error) {
+          console.error("Error loading snippet:", error);
+          showNotif("Error loading snippet", "error");
+        } finally {
+          setIsLoading(false);
+        }
       };
       loadSnippet();
     }
@@ -189,151 +199,164 @@ export const Builder = () => {
       <Navbar />
       {userProfile && (
         <div className="mb-8 flex h-full w-full flex-col-reverse gap-3 xl:flex-row">
-          <form className="flex h-full flex-col gap-5 rounded-sm xl:w-1/3">
-            <div className="flex flex-col">
-              <p className="text-sm text-base-300 dark:text-base-50">
-                NAME YOUR SNIPPET
-              </p>
-              <input
-                className="w-full bg-base-50 p-4 shadow-md focus:outline-none dark:bg-base-800 dark:text-base-50 dark:shadow-sm dark:shadow-base-600"
-                name="name"
-                value={snippet.name}
-                onChange={handleChange}
-              />
+          {isLoading ?
+            <div className="flex h-full w-full items-center justify-center">
+              <LoadingSpinner />
             </div>
-            <div>
-              <p className="text-sm text-base-300 dark:text-base-50">
-                DESCRIPTION
-              </p>
-              <textarea
-                className="h-[30svh] w-full resize-none rounded-sm bg-base-50 p-4 shadow-md focus:outline-none dark:bg-base-800 dark:text-base-50 dark:shadow-sm dark:shadow-base-600"
-                name="description"
-                value={snippet.description}
-                onChange={handleChange}
-              />
-            </div>
-            <div className="flex flex-col gap-2">
-              <p className="text-sm text-base-300 dark:text-base-50">
-                TAGS, COMMA SEPARATED
-              </p>
-              <input
-                className="w-full rounded-sm bg-base-50 p-4 shadow-md focus:outline-none dark:bg-base-800 dark:text-base-50 dark:shadow-sm dark:shadow-base-600"
-                name="tags"
-                value={snippet.tags}
-                onChange={handleChange}
-              />
-              <p className="text-sm text-base-300 dark:text-base-50">
-                THIS SNIPPET WILL BE ASSIGNED TO THESE CATEGORIES:
-              </p>
-              {categories && categories.length > 0 && (
-                <div className="flex flex-wrap gap-1">
-                  {categories.map((category, index) => (
-                    <span
-                      key={index}
-                      className="group relative flex h-8 items-center rounded-sm bg-base-950 px-2 py-1 text-xs text-base-50 dark:bg-base-50 dark:text-base-950"
-                    >
-                      {category.autoDetected &&
-                        category.kind === "language" && (
-                          <img
-                            src="/auto-sparkle.svg"
-                            className="mr-1 dark:invert"
-                          />
-                        )}
-                      {category.name}
-                      {category.autoDetected &&
-                        category.kind === "language" && (
-                          <span className="invisible absolute left-1/2 right-1/2 top-full mx-auto mt-2 w-max max-w-xs -translate-x-1/2 transform rounded-sm bg-base-950 p-3 text-xs text-base-50 group-hover:visible dark:invert">
-                            Language autodetected, you can overwrite this by
-                            entering the correct language into the tags
-                          </span>
-                        )}
-                    </span>
-                  ))}
-                </div>
-              )}
-            </div>
-            <div className="mt-auto flex w-full items-center justify-end gap-4">
-              <div className="flex h-full w-fit items-center self-center">
-                <div className="group relative flex h-full items-center shadow-md">
+          : <>
+              <form className="flex h-full flex-col gap-5 rounded-sm xl:w-1/3">
+                <div className="flex flex-col">
+                  <p className="text-sm text-base-300 dark:text-base-50">
+                    NAME YOUR SNIPPET
+                  </p>
                   <input
-                    type="checkbox"
-                    id="public"
-                    checked={snippet.public}
-                    onChange={handleCheckboxChange}
-                    className="peer sr-only"
+                    className="w-full bg-base-50 p-4 shadow-md focus:outline-none dark:bg-base-800 dark:text-base-50 dark:shadow-sm dark:shadow-base-600"
+                    name="name"
+                    value={snippet.name}
+                    onChange={handleChange}
                   />
-                  <label
-                    htmlFor="public"
-                    className="relative block aspect-square min-h-[3.75rem] cursor-pointer overflow-hidden rounded-sm bg-base-150 before:absolute before:inset-0 before:-translate-x-[110%] before:bg-blue-700 before:transition-transform before:duration-75 before:content-[''] peer-checked:before:translate-x-0 dark:border-base-600 dark:bg-base-850"
-                  >
-                    <img
-                      src={snippet.public ? "/lock-open.svg" : "/lock.svg"}
-                      alt="Lock"
-                      className={`absolute inset-0 h-full w-full p-4 ${snippet.public ? "invert-0" : "invert"} object-cover dark:invert-0`}
-                    />
-                  </label>
-                  <div className="absolute bottom-full left-1/2 mb-2 -translate-x-1/2 transform opacity-0 transition-opacity duration-75 group-hover:opacity-100">
-                    <div className="relative w-fit text-nowrap rounded-sm bg-gray-800 px-2 py-1 text-xs text-white">
-                      {snippet.public ?
-                        "Snippet is Public"
-                      : "Snippet is Private"}
-                      <svg
-                        className="absolute left-0 top-full h-2 w-full text-gray-800"
-                        x="0px"
-                        y="0px"
-                        viewBox="0 0 255 255"
-                        xmlSpace="preserve"
+                </div>
+                <div>
+                  <p className="text-sm text-base-300 dark:text-base-50">
+                    DESCRIPTION
+                  </p>
+                  <textarea
+                    className="h-[30svh] w-full resize-none rounded-sm bg-base-50 p-4 shadow-md focus:outline-none dark:bg-base-800 dark:text-base-50 dark:shadow-sm dark:shadow-base-600"
+                    name="description"
+                    value={snippet.description}
+                    onChange={handleChange}
+                  />
+                </div>
+                <div className="flex flex-col gap-2">
+                  <p className="text-sm text-base-300 dark:text-base-50">
+                    TAGS, COMMA SEPARATED
+                  </p>
+                  <input
+                    className="w-full rounded-sm bg-base-50 p-4 shadow-md focus:outline-none dark:bg-base-800 dark:text-base-50 dark:shadow-sm dark:shadow-base-600"
+                    name="tags"
+                    value={snippet.tags}
+                    onChange={handleChange}
+                  />
+                  <p className="text-sm text-base-300 dark:text-base-50">
+                    THIS SNIPPET WILL BE ASSIGNED TO THESE CATEGORIES:
+                  </p>
+                  {categories && categories.length > 0 && (
+                    <div className="flex flex-wrap gap-1">
+                      {categories.map((category, index) => (
+                        <span
+                          key={index}
+                          className="group relative flex h-8 items-center rounded-sm bg-base-950 px-2 py-1 text-xs text-base-50 dark:bg-base-50 dark:text-base-950"
+                        >
+                          {category.autoDetected &&
+                            category.kind === "language" && (
+                              <img
+                                src="/auto-sparkle.svg"
+                                className="mr-1 dark:invert"
+                              />
+                            )}
+                          {category.name}
+                          {category.autoDetected &&
+                            category.kind === "language" && (
+                              <span className="invisible absolute left-1/2 right-1/2 top-full mx-auto mt-2 w-max max-w-xs -translate-x-1/2 transform rounded-sm bg-base-950 p-3 text-xs text-base-50 group-hover:visible dark:invert">
+                                Language autodetected, you can overwrite this by
+                                entering the correct language into the tags
+                              </span>
+                            )}
+                        </span>
+                      ))}
+                    </div>
+                  )}
+                </div>
+                <div className="mt-auto flex w-full items-center justify-end gap-4">
+                  <div className="flex h-full w-fit items-center self-center">
+                    <div className="group relative flex h-full items-center shadow-md">
+                      <input
+                        type="checkbox"
+                        id="public"
+                        checked={snippet.public}
+                        onChange={handleCheckboxChange}
+                        className="peer sr-only"
+                      />
+                      <label
+                        htmlFor="public"
+                        className="relative block aspect-square min-h-[3.75rem] cursor-pointer overflow-hidden rounded-sm bg-base-150 before:absolute before:inset-0 before:-translate-x-[110%] before:bg-blue-700 before:transition-transform before:duration-75 before:content-[''] peer-checked:before:translate-x-0 dark:border-base-600 dark:bg-base-850"
                       >
-                        <polygon
-                          className="fill-current"
-                          points="0,0 127.5,127.5 255,0"
+                        <img
+                          src={snippet.public ? "/lock-open.svg" : "/lock.svg"}
+                          alt="Lock"
+                          className={`absolute inset-0 h-full w-full p-4 ${
+                            snippet.public ? "invert-0" : "invert"
+                          } object-cover dark:invert-0`}
                         />
-                      </svg>
+                      </label>
+                      <div className="absolute bottom-full left-1/2 mb-2 -translate-x-1/2 transform opacity-0 transition-opacity duration-75 group-hover:opacity-100">
+                        <div className="relative w-fit text-nowrap rounded-sm bg-gray-800 px-2 py-1 text-xs text-white">
+                          {snippet.public ?
+                            "Snippet is Public"
+                          : "Snippet is Private"}
+                          <svg
+                            className="absolute left-0 top-full h-2 w-full text-gray-800"
+                            x="0px"
+                            y="0px"
+                            viewBox="0 0 255 255"
+                            xmlSpace="preserve"
+                          >
+                            <polygon
+                              className="fill-current"
+                              points="0,0 127.5,127.5 255,0"
+                            />
+                          </svg>
+                        </div>
+                      </div>
                     </div>
                   </div>
+                  <button
+                    onClick={handleSubmit}
+                    disabled={!snippet.name || !snippet.code}
+                    className="group relative w-1/2 self-center overflow-hidden rounded-sm p-4 text-base-950 shadow-md duration-75 hover:cursor-pointer hover:text-base-50 disabled:invert-[45%] dark:bg-base-800 dark:text-base-50 dark:shadow-sm dark:shadow-base-600"
+                  >
+                    <div
+                      className={`${
+                        isCreator ? "bg-blue-700" : "bg-red-600"
+                      } absolute inset-0 -translate-x-[110%] transform transition-transform duration-75 ease-in-out group-hover:translate-x-0`}
+                      aria-hidden="true"
+                    />
+                    <span className="relative z-10 text-xl font-bold">
+                      {isEditing ?
+                        isCreator ?
+                          "SAVE"
+                        : "YOU ARE NOT THE AUTHOR"
+                      : "CREATE"}
+                    </span>
+                  </button>
                 </div>
-              </div>
-              <button
-                onClick={handleSubmit}
-                disabled={!snippet.name || !snippet.code}
-                className="group relative w-1/2 self-center overflow-hidden rounded-sm p-4 text-base-950 shadow-md duration-75 hover:cursor-pointer hover:text-base-50 disabled:invert-[45%] dark:bg-base-800 dark:text-base-50 dark:shadow-sm dark:shadow-base-600"
-              >
-                <div
-                  className={`${isCreator ? "bg-blue-700" : "bg-red-600"} absolute inset-0 -translate-x-[110%] transform transition-transform duration-75 ease-in-out group-hover:translate-x-0`}
-                  aria-hidden="true"
+              </form>
+              <div className="flex h-[70svh] w-full shadow-md xl:h-full xl:w-2/3">
+                <Editor
+                  height="100%"
+                  value={snippet.code}
+                  width="100%"
+                  options={{
+                    scrollBeyondLastLine: true,
+                    fontSize: 15,
+                    padding: {
+                      top: 30,
+                    },
+                  }}
+                  theme={selectedStyle}
+                  defaultLanguage="auto"
+                  onChange={handleCodeChange}
                 />
-                <span className="relative z-10 text-xl font-bold">
-                  {isEditing ?
-                    isCreator ?
-                      "SAVE"
-                    : "YOU ARE NOT THE AUTHOR"
-                  : "CREATE"}
-                </span>
-              </button>
-            </div>
-          </form>
-          <div className="flex h-[70svh] w-full shadow-md xl:h-full xl:w-2/3">
-            <Editor
-              height="100%"
-              value={snippet.code}
-              width="100%"
-              options={{
-                scrollBeyondLastLine: true,
-                fontSize: 15,
-                padding: {
-                  top: 30,
-                },
-              }}
-              theme={selectedStyle}
-              defaultLanguage="auto"
-              onChange={handleCodeChange}
-            />
-          </div>
+              </div>
+            </>
+          }
         </div>
       )}
       {!userProfile && (
         <div className="mb-8 flex h-full w-full flex-col items-center justify-center gap-3">
-          <h1 className="bg-red-500 p-5 text-2xl text-base-50">{`Please sign in to ${isEditing ? "edit" : "create"}`}</h1>
+          <h1 className="bg-red-500 p-5 text-2xl text-base-50">{`Please sign in to ${
+            isEditing ? "edit" : "create"
+          }`}</h1>
         </div>
       )}
 
