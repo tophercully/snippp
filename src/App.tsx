31dab3d3e47d0bbe0408c7f96cd86d3a90c485cb
tophercully <89924670+tophercully@users.noreply.github.com>
2024-08-04 14:45:25 +0200

    clean up homepage code

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,12 @@
-import { useState, useEffect } from "react";
 import { Footer } from "./components/nav/Footer";
 import { Navbar } from "./components/nav/Navbar";
-import { fetchStats } from "./backend/fetchStats";
+
 import "./output.css";
 import { useNavigate } from "react-router-dom";
 
 const Home = () => {
   document.title = `Snippp - Snippet Organizer`;
   const navigate = useNavigate();
-  const [stats, setStats] = useState<{
-    snippetCount: number;
-    userCount: number;
-  } | null>(null);
-
-  useEffect(() => {
-    const getStats = async () => {
-      try {
-        const info = await fetchStats();
-        setStats(info);
-      } catch (error) {
-        console.error("Error fetching homepage info:", error);
-      }
-    };
-
-    getStats();
-  }, []);
 
   return (
     <div className="relative flex h-fit min-h-[100svh] w-[99svw] flex-col items-center justify-center bg-base-50 p-2 pb-32 pt-32 font-satoshi md:p-10 dark:bg-base-950">
@@ -40,76 +22,43 @@ const Home = () => {
         <div className="absolute inset-0 -z-10 m-[-6px] border-8 border-transparent" />
 
         <div className="flex w-fit max-w-[60ch] flex-col gap-4 p-4 text-left text-sm md:mt-24 md:p-0 md:text-2xl dark:text-base-400">
-          {/* <h2 className="max-w-[40ch] text-lg font-medium text-base-950 dark:text-base-50">
-            Get in, Get out, Get back to building
-          </h2> */}
           <h2 className="max-w-[40ch] text-xl font-medium text-base-950 dark:text-base-50">
             Sort Smarter, Build Faster
           </h2>
           <p className="text-base">Your toolkit, amplified.</p>
-          {/* <p className="text-base">
-            Our lightweight tool ensures you spend less time searching and more
-            time coding.
-          </p> */}
-          {/* <h2 className="max-w-[80ch] text-2xl font-bold text-base-950 dark:text-base-50">
-            Get in, Get Out, Get Back to Coding{" "}
-          </h2> */}
 
           <p className="text-base">
             Organize, share, and discover code snippets FAST, so you can get
             back to coding.
           </p>
-          {/* <p className="">Streamline your workflow with:</p> */}
-
-          {/* <ul className="flex flex-col gap-2 text-base font-light text-base-950 dark:text-base-50">
-            <li>
-              <span className="mr-2 font-medium">
-                Lightning-fast Navigation:
-              </span>
-              Quickly find and use the code you need.
-            </li>
-            <li>
-              <span className="mr-2 font-medium">Intuitive Organization:</span>
-              Effortlessly categorize and manage your snippets.
-            </li>
-            <li>
-              <span className="mr-2 font-medium">Easy Sharing:</span>
-              Collaborate with your team or the community.
-            </li>
-            <li>
-              <span className="mr-2 font-medium">Endless Discovery: </span>
-              Explore a vast library of snippets shared by other developers.
-            </li>
-          </ul> */}
         </div>
       </div>
-      {/* <h3 className="mt-12 w-full max-w-96 text-center text-xl text-base-500 md:mt-24 md:text-2xl dark:text-base-400">
-        UNINTERRUPTED CREATIVITY FOR CREATIVE CODERS – YOUR SNIPPET ORGANIZER
-      </h3> */}
-      <a
-        href="/browse"
-        className={`${stats ? "opacity-100" : "opacity-0"} group relative mt-12 flex items-center justify-center overflow-hidden rounded-sm bg-base-950 px-4 text-base-50 shadow-md duration-75 md:mt-24 dark:bg-base-50 dark:text-base-950 dark:shadow-sm dark:shadow-base-600`}
-      >
-        <div
-          className="absolute inset-0 -translate-x-full transform bg-blue-700 transition-transform duration-75 ease-in-out group-hover:translate-x-0 dark:text-base-950"
-          aria-hidden="true"
-        />
-        <span className="relative z-10 text-xl md:text-2xl dark:group-hover:text-base-50">
-          {`BROWSE ${stats?.snippetCount ?? ""} SNIPPPETS`}
-        </span>
-        <img
-          src="arrow-right-long.svg"
-          className="relative ml-3 h-14 dark:invert dark:group-hover:invert-0"
-        />
-      </a>
-      <button
-        className="my-4 mb-24 text-sm text-base-500"
-        onClick={() => navigate("/stats")}
-      >
-        more stats
-      </button>
+      <div className={`flex flex-col gap-4 lg:flex-row`}>
+        <a
+          href="/browse"
+          className={`group relative mt-4 flex items-center justify-center overflow-hidden rounded-sm bg-base-950 px-4 py-1 text-base-50 shadow-md duration-75 md:mt-24 dark:bg-base-50 dark:text-base-950 dark:shadow-sm dark:shadow-base-600`}
+        >
+          <div
+            className="absolute inset-0 -translate-x-full transform bg-blue-700 transition-transform duration-75 ease-in-out group-hover:translate-x-0 dark:text-base-950"
+            aria-hidden="true"
+          />
+          <span className="relative z-10 text-xl md:text-2xl dark:group-hover:text-base-50">
+            {`Browse tools from the community`}
+          </span>
+          <img
+            src="arrow-right-long.svg"
+            className="relative ml-3 h-10 dark:invert dark:group-hover:invert-0"
+          />
+        </a>
+      </div>
 
       <div className="absolute bottom-0 w-[95%] p-2">
+        <button
+          className="my-4 justify-self-end text-sm text-base-500"
+          onClick={() => navigate("/stats")}
+        >
+          stats
+        </button>
         <Footer />
       </div>
     </div>
