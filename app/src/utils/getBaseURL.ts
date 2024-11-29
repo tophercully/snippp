a2a8b90d5b26e04afa3294a0395d0d047308ce23
tophercully <89924670+tophercully@users.noreply.github.com>
2024-11-29 16:29:50 -0500

    new baseUrl function

--- app/src/utils/getBaseURL.ts
+++ app/src/utils/getBaseURL.ts
@@ -1,6 +1,10 @@
 const getBaseURL = () => {
-  const prodUrl = "https://" + process.env.VERCEL_URL;
-  const devUrl = "http://localhost:3000";
-  return process.env.NODE_ENV === "production" ? prodUrl : devUrl;
+  if (process.env.NODE_ENV === "development") {
+    return "http://localhost:3000";
+  }
+
+  // Use the Vercel-provided deployment URL for preview and production
+  return `https://${process.env.VERCEL_BRANCH_URL || process.env.VERCEL_URL}`;
 };
+
 export default getBaseURL;
