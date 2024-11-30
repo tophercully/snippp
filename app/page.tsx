942610c2d210833eaf52fa7df1eb2bd566aef0bc
tophercully <89924670+tophercully@users.noreply.github.com>
2024-11-30 10:58:28 -0500

    accessibility improvements

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,3 @@
-
 import "./output.css";
 import SnipppButton from "./src/components/universal/SnipppButton";
 import SubtleGridBackground from "./src/components/universal/SubtleGridBackground";
@@ -8,9 +7,6 @@ import Link from "next/link";
 import CTA from "./src/components/home/CTA";
 
 const Home = () => {
-
-
-
   return (
     <div className="font-satoshi relative flex h-fit min-h-[100svh] w-[100svw] flex-col items-center justify-center gap-12 p-2 pb-32 pt-32 md:p-16">
       <Navbar />
@@ -24,11 +20,11 @@ const Home = () => {
             id="hero-text"
             className="z-10 flex h-fit flex-col items-start justify-center gap-8 p-2 md:gap-16"
           >
-            <h2 className="flex flex-col self-start text-7xl font-bold text-base-950 md:max-w-[66vw] md:text-9xl dark:text-base-50">
+            <h1 className="flex flex-col self-start text-7xl font-bold text-base-950 md:max-w-[66vw] md:text-9xl dark:text-base-50">
               <span className="w-fit bg-info p-1 text-white">Your</span>
               <span className="w-fit bg-info p-1 text-white">toolkit,</span>
               <span className="w-fit bg-info p-1 text-white">amplified</span>
-            </h2>
+            </h1>
             <div
               id="desc"
               className="text-md flex w-fit flex-col text-left text-black md:p-0 md:text-4xl"
@@ -42,6 +38,7 @@ const Home = () => {
           <div className="relative hidden h-1/2 w-2/3 flex-col items-center justify-center xl:flex">
             <img
               src="dashboardMockup2.png"
+              alt="Snippp Dashboard displayed on a laptop screen"
               className="z-20"
             />
           </div>
@@ -50,7 +47,7 @@ const Home = () => {
           id="CTA"
           className="z-10 flex items-center gap-4 self-center md:self-end"
         >
-          <CTA/>
+          <CTA />
           <SnipppButton
             href="/browse"
             size="lg"
@@ -63,6 +60,7 @@ const Home = () => {
               Browse tools
               <img
                 src="arrow-right-long.svg"
+                alt="Right arrow"
                 className="relative ml-3 h-6 invert group-hover:invert-0 dark:invert-0"
               />
             </span>
@@ -73,13 +71,13 @@ const Home = () => {
       <div className="flex min-h-[70svh] w-full flex-col items-center justify-center">
         <div className="flex h-full w-full flex-col items-center justify-around gap-8 xl:flex-row">
           <div className="flex flex-col gap-8 p-2 text-black md:gap-16 dark:text-white">
-            <h1 className="flex w-2/3 flex-col text-7xl font-bold text-black md:text-9xl dark:text-white">
+            <h2 className="flex w-2/3 flex-col text-7xl font-bold text-black md:text-9xl dark:text-white">
               <span className="w-fit bg-error p-1 text-white">Sort</span>
               <span className="w-fit bg-error p-1 text-white">smarter,</span>
               <span className="w-fit text-nowrap bg-error p-1 text-white">
                 build faster
               </span>
-            </h1>
+            </h2>
             <p className="text-xl backdrop-blur-sm md:text-3xl">
               Create tailored lists to keep your go-to snippets within reach—
               boost your productivity and never waste time searching again.
@@ -87,6 +85,7 @@ const Home = () => {
           </div>
           <img
             src="/listsScreenshot.png"
+            alt="Screenshot of Lists View"
             className="max-w-[70vw] rounded-lg shadow-xl md:max-w-[35vw]"
           />
         </div>
@@ -95,7 +94,7 @@ const Home = () => {
       <div className="mb-12 flex min-h-[90svh] w-full flex-col items-center justify-center">
         <div className="flex h-full w-full flex-col items-center justify-around gap-8 xl:flex-row">
           <div className="flex flex-col gap-8 text-black md:gap-16 dark:text-white">
-            <h1 className="flex w-2/3 flex-col p-2 text-7xl font-bold text-black md:text-9xl dark:text-white">
+            <h2 className="flex w-2/3 flex-col p-2 text-7xl font-bold text-black md:text-9xl dark:text-white">
               <span className="z-10 w-fit bg-success p-1 text-white">
                 Leverage
               </span>
@@ -103,7 +102,7 @@ const Home = () => {
                 collective
               </span>
               <span className="w-fit bg-success p-1 text-white">knowledge</span>
-            </h1>
+            </h2>
             <p className="p-2 text-xl backdrop-blur-sm md:text-3xl">
               Explore a rich library of snippets from developers around the
               world or share your own with the community. Learning and
@@ -117,6 +116,7 @@ const Home = () => {
           <div className="w-full p-2 md:p-0">
             <img
               src="/profileScreenshot.png"
+              alt="Screenshot of Snippp profile"
               className="md:max-h-2/3 rounded-lg shadow-xl md:max-w-[35vw]"
             />
           </div>
@@ -126,7 +126,7 @@ const Home = () => {
       <div className="mb-12 flex min-h-[90svh] w-full flex-col items-center justify-center">
         <div className="flex h-full w-full flex-col items-center justify-around gap-8 xl:flex-row">
           <div className="flex flex-col gap-8 text-black md:gap-16 dark:text-white">
-            <h1 className="flex w-2/3 flex-col p-2 text-7xl font-bold text-black md:text-9xl dark:text-white">
+            <h2 className="flex w-2/3 flex-col p-2 text-7xl font-bold text-black md:text-9xl dark:text-white">
               <span className="flex w-fit flex-wrap">
                 <span className="w-fit bg-special p-1 text-white">Your</span>
                 <span className="w-fit bg-special p-1 text-white">code,</span>
@@ -137,7 +137,7 @@ const Home = () => {
                 </span>
                 <span className="w-fit bg-special p-1 text-white">control</span>
               </span>
-            </h1>
+            </h2>
             <p className="p-2 text-xl backdrop-blur-sm md:text-3xl">
               We prioritize your privacy. Control who sees your snippets with
               customizable privacy settings, ensuring your code is safe and
@@ -160,10 +160,12 @@ const Home = () => {
             <div className="flex w-full flex-col gap-4 p-2 md:p-0">
               <img
                 src="/privacyScreenshot.png"
+                alt="Screenshot of a snippet marked as private"
                 className="md:max-h-2/3 rounded-lg shadow-xl md:max-w-[35vw]"
               />
               <img
                 src="/privateScreenshot.png"
+                alt="Screenshot of Privacy Settings"
                 className="md:max-h-2/3 rounded-lg shadow-xl md:max-w-[35vw]"
               />
             </div>
@@ -173,17 +175,17 @@ const Home = () => {
       <div className="mb-12 flex min-h-[90svh] w-full flex-col items-center justify-center">
         <div className="flex h-full w-full flex-col items-center justify-around gap-8 xl:flex-row">
           <div className="flex flex-col gap-8 text-black md:gap-16 dark:text-white">
-            <h1 className="flex w-2/3 flex-col p-2 text-7xl font-bold text-black md:text-9xl dark:text-white">
+            <h2 className="flex w-2/3 flex-col p-2 text-7xl font-bold text-black md:text-9xl dark:text-white">
               <span className="w-fit bg-info p-1 text-white">Start </span>
               <span className="w-fit bg-info p-1 text-white">for </span>
               <span className="w-fit bg-info p-1 text-white">$free</span>
-            </h1>
+            </h2>
             <p className="p-2 text-xl backdrop-blur-sm md:text-3xl">
               Sign up now and join a growing community of developers optimizing
               their workflow with our powerful snippet organizer.
             </p>
             <div className="self-center">
-              <CTA/>
+              <CTA />
             </div>
             <p className="p-2 text-xl backdrop-blur-sm md:text-3xl">
               Unlock advanced features and unlimited storage with our upcoming
@@ -193,7 +195,8 @@ const Home = () => {
         </div>
       </div>
       <div className="absolute bottom-0 w-[95%] p-2">
-        <Link href="/stats"
+        <Link
+          href="/stats"
           className="my-4 justify-self-end text-sm text-base-500"
         >
           stats
