2cf0e253532e50bbaceb59a98250bb92f94fb516
tophercully <89924670+tophercully@users.noreply.github.com>
2025-02-19 11:35:20 -0500

    ads.txt, update roadmap, styling fixes, browser refactor

--- app/src/pages/BrowserContent.tsx
+++ app/src/pages/BrowserContent.tsx
@@ -7,178 +7,148 @@ import { Footer } from "@/app/src/components/nav/Footer";
 import { Navbar } from "@/app/src/components/nav/Navbar";
 import { LoadingSpinner } from "@/app/src/components/universal/LoadingSpinner";
 import { categories } from "@/app/src/data/categories";
-import { Snippet } from "@/app/src/types/typeInterfaces";
+import { Snippet, SnippetMod } from "@/app/src/types/typeInterfaces";
 import searchAndScoreSnippets from "@/app/src/utils/Search";
 import searchSuggestions from "@/app/src/utils/searchSuggestions";
 import { setPageTitle } from "@/app/src/utils/setPageTitle";
 import Link from "next/link";
-import { useParams } from "next/navigation";
-import React, { useState, useEffect, useCallback, useMemo } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import GoogleAd from "../components/ads/GoogleAd";
+import { computed, effect, signal } from "@preact-signals/safe-react";
+import sortByProperty from "../utils/sortByProperty";
 
 type SortOrder = "asc" | "desc";
 type SortableSnippetKeys = keyof Snippet;
-
-function sortByProperty<T>(
-  array: T[],
-  property: keyof T,
-  order: SortOrder = "asc",
-): T[] {
-  if (property === "name") {
-    return array.slice().sort((a, b) => {
-      if (a[property] < b[property]) {
-        return order === "desc" ? -1 : 1;
-      } else if (a[property] > b[property]) {
-        return order === "desc" ? 1 : -1;
-      } else {
-        return 0;
-      }
-    });
-  } else {
-    return array.slice().sort((a, b) => {
-      if (a[property] < b[property]) {
-        return order === "asc" ? -1 : 1;
-      } else if (a[property] > b[property]) {
-        return order === "asc" ? 1 : -1;
-      } else {
-        return 0;
-      }
-    });
-  }
-}
-
-type SnippetMod = {
-  favoriteStatus?: boolean;
-  favoriteCount?: number;
-  copyCount?: number;
-  isDeleted?: boolean;
-};
-
 type SnippetMods = { [snippetID: number]: SnippetMod };
 
 const shuffledSuggestions = searchSuggestions.sort(() => Math.random() - 0.5);
 const searchAllPlaceholder = `search - try "${shuffledSuggestions[0]}" or "${shuffledSuggestions[1]}"`;
 
-const BrowserContent: React.FC = () => {
-  const { category } = useParams();
+const snippets = signal<Snippet[]>([]);
+const snippetMods = signal<SnippetMods>({});
+const query = signal<string>("");
+const sortMethod = signal<SortableSnippetKeys>("copyCount");
+const sortOrder = signal<SortOrder>("desc");
+const category = window.location.pathname.split("/").slice(2, 3)[0] || "";
+console.log(`category: ${category}`);
+const filteredAndSortedSnippets = computed(() => {
+  let filteredSnippets = snippets.value.filter(
+    (snippet) => !snippetMods.peek()[snippet.snippetID]?.isDeleted,
+  );
 
-  const [snippets, setSnippets] = useState<Snippet[]>([]);
-  const [snippetMods, setSnippetMods] = useState<SnippetMods>({});
-  const [selection, setSelection] = useState<Snippet | null>(null);
-  const [query, setQuery] = useState<string>("");
-  if (category) {
-    setPageTitle(`${categories[category[0]].name} Snippets`);
-  } else {
-    setPageTitle("All Snippets");
-  }
-  const [sortMethod, setSortMethod] =
-    useState<SortableSnippetKeys>("copyCount");
-  const [sortOrder, setSortOrder] = useState<SortOrder>("desc");
-  const [isLoading, setIsLoading] = useState(true);
-  const [isTransitioning, setIsTransitioning] = useState(false);
-
-  const fetchSnippets = useCallback(async () => {
-    setIsLoading(true);
-    setIsTransitioning(true);
-    const userProfile = JSON.parse(
-      localStorage.getItem("userProfile") || "null",
+  if (category && categories[category[0]]) {
+    const categoryTags = categories[category[0]].tags;
+    filteredSnippets = filteredSnippets.filter((snippet) =>
+      categoryTags.some((catTag) =>
+        snippet.tags
+          .toLowerCase()
+          .split(",")
+          .map((tag) => tag.trim())
+          .includes(catTag),
+      ),
     );
-    const userID = userProfile ? userProfile.id : undefined;
-    const snippetsArray = await loadAllSnippets(userID);
-    setSnippets(snippetsArray);
-    setSnippetMods({});
-    setIsLoading(false);
-    setTimeout(() => setIsTransitioning(false), 300);
+  }
+
+  if (query) {
+    return searchAndScoreSnippets(query.value, filteredSnippets);
+  }
+
+  return sortByProperty(filteredSnippets, sortMethod.value, sortOrder.value);
+});
+const selection = signal<Snippet | null>(null);
+const isLoading = signal<boolean>(true);
+const isTransitioning = signal<boolean>(false);
+
+const BrowserContent: React.FC = () => {
+  useEffect(() => {
+    if (category) {
+      setPageTitle(
+        `${categories[category] ? categories[category].name : "All"} Snippets`,
+      );
+    } else {
+      setPageTitle("All Snippets");
+    }
   }, []);
+
+  const fetchSnippets = async () => {
+    try {
+      isLoading.value = true;
+      isTransitioning.value = true;
+      const userProfile = JSON.parse(
+        localStorage.getItem("userProfile") || "null",
+      );
+      const userID = userProfile ? userProfile.id : undefined;
+      const snippetsArray = await loadAllSnippets(userID);
+      console.log(`Setting snippets to ${snippetsArray.length} snippets`);
+      snippets.value = snippetsArray;
+      snippetMods.value = {}; // Reset snippet mods when snippets change
+      isLoading.value = false;
+      setTimeout(() => (isTransitioning.value = false), 300);
+    } catch (error) {
+      console.error("Error fetching snippets:", error);
+    }
+  };
 
   useEffect(() => {
     fetchSnippets();
-  }, [fetchSnippets]);
+  }, []);
 
-  const updateSnippetMod = useCallback(
-    (id: number, mod: Partial<SnippetMod>) => {
-      setSnippetMods((prevMods) => ({
-        ...prevMods,
-        [id]: { ...prevMods[id], ...mod },
-      }));
-    },
-    [],
-  );
-
-  const filteredAndSortedSnippets = useMemo(() => {
-    let filteredSnippets = snippets.filter(
-      (snippet) => !snippetMods[snippet.snippetID]?.isDeleted,
-    );
-
-    if (category && categories[category[0]]) {
-      const categoryTags = categories[category[0]].tags;
-      filteredSnippets = filteredSnippets.filter((snippet) =>
-        categoryTags.some((catTag) =>
-          snippet.tags
-            .toLowerCase()
-            .split(",")
-            .map((tag) => tag.trim())
-            .includes(catTag),
-        ),
-      );
-    }
-
-    if (query) {
-      return searchAndScoreSnippets(query, filteredSnippets);
-    }
-
-    return sortByProperty(filteredSnippets, sortMethod, sortOrder);
-  }, [snippets, snippetMods, query, sortMethod, sortOrder, category]);
+  const updateSnippetMod = (id: number, mod: Partial<SnippetMod>) => {
+    snippetMods.value = {
+      ...snippetMods.peek(),
+      [id]: { ...snippetMods.peek()[id], ...mod },
+    };
+  };
 
   const isReady =
-    !isLoading && filteredAndSortedSnippets.length > 0 && selection;
+    !isLoading.value && filteredAndSortedSnippets.value.length > 0;
 
   return (
     <div className="over flex h-screen w-screen flex-col bg-base-100 p-2 pt-24 lg:p-10 lg:pt-24 dark:bg-base-900">
       <Navbar />
-      {/* <GoogleAd /> */}
       <GoogleAd adSlot="6797801111" />
       <div className="flex h-fit w-full flex-col-reverse shadow-lg lg:h-[96%] lg:flex-row">
         <div className="flex h-full w-full flex-col lg:w-1/3">
           <SearchBar
-            query={query}
-            setQuery={setQuery}
+            query={query.value}
+            setQuery={(newQuery) => (query.value = newQuery as string)}
             placeHolder={
               category ?
-                `search ${categories[category[0]].name.toLowerCase()} snipppets`
+                `search ${categories[category]?.name.toLowerCase()} snipppets`
               : searchAllPlaceholder
             }
-            setSortMethod={setSortMethod}
-            sortMethod={sortMethod}
-            sortOrder={sortOrder}
-            setSortOrder={setSortOrder}
+            setSortMethod={(newValue) => (sortMethod.value = newValue)}
+            sortMethod={sortMethod.value}
+            sortOrder={sortOrder.value}
+            setSortOrder={(newSortOrder) =>
+              (sortOrder.value = newSortOrder as SortOrder)
+            }
           />
           <div className="relative h-full w-full overflow-hidden">
             <div
               className={`absolute inset-0 flex items-center justify-center transition-opacity duration-75 ${
-                isLoading || isTransitioning ? "opacity-100" : (
-                  "pointer-events-none opacity-0"
-                )
+                isLoading.value || isTransitioning.value ?
+                  "opacity-100"
+                : "pointer-events-none opacity-0"
               }`}
             >
-              {/* <p className="text-xl text-base-950 dark:text-base-50">
-                Loading...
-              </p> */}
               <LoadingSpinner />
             </div>
             <div
               className={`h-full w-full transition-opacity duration-75 ${
-                isLoading || isTransitioning ?
+                isLoading.value || isTransitioning.value ?
                   "pointer-events-none opacity-0"
                 : "opacity-100"
               }`}
             >
-              {filteredAndSortedSnippets.length > 0 ?
+              {filteredAndSortedSnippets.value.length > 0 ?
                 <ListSnippets
-                  snippets={filteredAndSortedSnippets}
-                  snippetMods={snippetMods}
-                  selection={selection}
-                  setSelection={setSelection}
+                  snippets={filteredAndSortedSnippets.value}
+                  snippetMods={snippetMods.value}
+                  selection={selection.value}
+                  setSelection={(newSelection) =>
+                    (selection.value = newSelection as Snippet)
+                  }
                 />
               : <div className="flex h-full flex-col items-center gap-5">
                   <h1 className="mt-4 p-2 text-base-950 dark:text-base-50">
@@ -200,11 +170,11 @@ const BrowserContent: React.FC = () => {
             isReady ? "opacity-100" : "pointer-events-none opacity-0"
           }`}
         >
-          {isReady && (
+          {isReady && selection.value && (
             <Display
-              selection={selection}
+              selection={selection.value as Snippet}
               updateSnippetMod={updateSnippetMod}
-              snippetMods={snippetMods}
+              snippetMods={snippetMods.value}
             />
           )}
         </div>
